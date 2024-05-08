6013d8ecd3a0d0304338a61d4405eb214d380586
josec211 <168625477+josec211@users.noreply.github.com>
2024-05-07 22:53:32 -0600

    Update info-firstchat.js

--- plugins/info-firstchat.js
+++ plugins/info-firstchat.js
@@ -7,12 +7,10 @@ if (m.chat.endsWith('broadcast') || m.fromMe || m.isGroup) return
 let user = global.db.data.users[m.sender]
 
 if (new Date() - user.pc < 21600000) return
-await m.reply(`🌟 *Hola ${username}, bienvenido a KatashiBot* 🥷🏻
+await m.reply(`🌟 *Hola ${username}, bienvenido a BOT-JC-LM* 🥷🏻
 📲 _Ejecute el comando .menu para visualizar los comandos que dispongo_ 🫡
 
 📌 *Cualquier duda o sugerencia puede contactar a mi creador:* 🎉
-📩 wa.me/51948705559 📭
-🙏❣️ *Si desea apoyar el proyecto Katashi Bot puede donar a esta cuenta por PayPal* ✅📈
-🌟 https://www.paypal.com/paypalme/KatashiF ✨`) 
+📩 wa.me/50493108279 📭✨`) 
 user.pc = new Date * 1
 }
